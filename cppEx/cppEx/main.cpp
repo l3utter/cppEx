62d886a74cf7cb180e4e3ebe036fb6e818540fb3
l3utter <kwanchinee@gmail.com>
2021-02-23 21:31:07 +0800

    Initial commit

---
+++ cppEx/cppEx/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppEx
+//
+//  Created by Ryan Ng on 23/2/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
